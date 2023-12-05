aad0ae860d49c59734a61e52926eb983154d3105
viacoro <hoellerl.adam@gmail.com>
2023-12-05 15:11:39 +0100

    first test

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,9 +1,18 @@
 import { logger } from "@vendetta";
 import Settings from "./Settings";
+import { findByProps } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
+
+const MediaManager = findByProps("downloadMediaAsset");
+let patches = [];
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        patches.push(
+            before("downloadMediaAsset", MediaManager, (args) => {
+                logger.log("Downloading media asset: " + args[0]);
+            })
+        );
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
