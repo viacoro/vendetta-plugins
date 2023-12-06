d7dd4fc550c146094b22d1e60174e44cd6679d51
viacoro <hoellerl.adam@gmail.com>
2023-12-06 11:27:14 +0100

    more testing

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -6,16 +6,15 @@ import { before } from "@vendetta/patcher";
 const MediaManager = findByProps("downloadMediaAsset");
 let patches = [];
 logger.log("Hello, world first");
+console.log("Hello, world first");
 export default {
     onLoad: () => {
         logger.log("Hello, world second");
         console.log("Hello, world second");
-        patches.push(
-            before("downloadMediaAsset", MediaManager, (args) => {
-                logger.log("Downloading media asset: " + args[0]);
-                console.log("Downloading media asset: " + args[0]);
-            })
-        );
+        before("downloadMediaAsset", MediaManager, (args) => {
+            logger.log("Downloading media asset: " + args[0]);
+            console.log("Downloading media asset: " + args[0]);
+        });
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
